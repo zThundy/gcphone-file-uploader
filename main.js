aa277e180358041755e42b224aa77cbc0f1f1326
zThundy <antonioantonelli7@gmail.com>
2021-07-03 17:56:28 +0200

    changes allowing every connection to express

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ if (!String.prototype.format) {
 }
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + REMOTE_PORT);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
